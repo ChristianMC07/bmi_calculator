03a42ce6b143acad570b5e6ac6ef4d0d39c4eb20
ChristianMC07 <christian.marcelo.castro@gmail.com>
2024-03-29 23:52:32 -0300

    extracting the card class

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,12 +14,66 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    myColor: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    myColor: Color(0xFF1D1E33),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              myColor: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(myColor: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(myColor: Color(0xFF1D1E33)),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: null,
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color myColor;
+
+  const ReusableCard({
+    super.key,
+    required this.myColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: myColor,
       ),
     );
   }
