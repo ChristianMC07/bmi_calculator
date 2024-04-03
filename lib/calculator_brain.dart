b0ba9b84e1cac3e9168403c82bfe019d009af2d9
ChristianMC07 <christian.marcelo.castro@gmail.com>
2024-04-03 14:07:18 -0300

    calculating the BMI

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -1,6 +1,13 @@
+import 'dart:math';
+
 class CalculatorBrain {
   final int height;
   final int weight;
 
   CalculatorBrain({required this.height, required this.weight});
+
+  String calculateBMI() {
+    double bmi = weight / pow(height / 100, 2);
+    return bmi.toStringAsFixed(1);
+  }
 }
